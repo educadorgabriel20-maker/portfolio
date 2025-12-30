7c543582abfa72f87e2bee86e006574a28667812
educadorgabriel20-maker <educador.gabriel20@gmail.com>
2025-12-30 19:32:27 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const toggleButton = document.getElementById("theme-toggle");
-const html = document.documentElement;
-
-toggleButton.addEventListener("click", () => {
-    const isDark = html.getAttribute("data-theme") === "dark";
-    html.setAttribute("data-theme", isDark ? "light" : "dark");
-    toggleButton.textContent = isDark ? "☀️" : "🌙";
-});
